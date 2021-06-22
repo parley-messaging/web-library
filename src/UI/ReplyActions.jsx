5f33ddddd83c29028db76a6eed19cf58621e368b
Gerben-T <gerben.tesselaar@tracebuzz.com>
2021-06-22 16:49:18 +0200

    Remove comment

--- src/UI/ReplyActions.jsx
+++ src/UI/ReplyActions.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import styles from "./ReplyActions.module.css";
-
-// components
 import ReplyText from "./ReplyText";
 import MobileSubmit from "./Buttons/MobileSubmit";
 
