17eadd67173ec9154d40a7e8758d5a6394ad7012
Gerben-T <gerben.tesselaar@tracebuzz.com>
2021-06-22 14:35:25 +0200

    Remove comment

--- src/UI/Buttons/HeaderButton.jsx
+++ src/UI/Buttons/HeaderButton.jsx
@@ -2,8 +2,6 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import styles from "./HeaderButton.module.css";
 import {BUTTONMENU, BUTTONMINIMIZE, BUTTONCLOSE} from "../interfaceTexts.js";
-
-// Requirements
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars} from "@fortawesome/free-solid-svg-icons/faBars";
 import {faWindowMinimize} from "@fortawesome/free-regular-svg-icons/faWindowMinimize";
