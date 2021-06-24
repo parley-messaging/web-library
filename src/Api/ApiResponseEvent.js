ec193c1746646ef1409796d0f6181fb13fa3e1d5
Gerben-T <gerben.tesselaar@tracebuzz.com>
2021-06-24 14:34:23 +0200

    Remove files which are removed/moved on master

--- src/Api/ApiResponseEvent.js
+++
@@ -1,9 +0,0 @@
-// This script contains the custom events that are fired
-// in the ApiEventTarget.js script.
-// These event objects are passed along to the event listeners.
-
-export default class ApiResponseEvent extends CustomEvent {
-	constructor(eventName, data) {
-		super(eventName, {detail: data});
-	}
-}
--- src/Api/Config.js
+++
@@ -1,9 +0,0 @@
-// This script contains a simple object for storing API configurations.
-// It is used whenever an API call is made so it knows where to send the request.
-
-export default class Config {
-	constructor(apiDomain = "https://api.parley.nu") {
-		this.apiDomain = apiDomain;
-		this.apiUrl = `${apiDomain}/clientApi/v1.6`;
-	}
-}
