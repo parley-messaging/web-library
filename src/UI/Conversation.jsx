3f147dcadf0b98fa1f5d9cda373e9bdb1a4582ac
Gerben-T <gerben.tesselaar@tracebuzz.com>
2021-07-01 16:52:46 +0200

    Show messages in conversation

--- src/UI/Conversation.jsx
+++ src/UI/Conversation.jsx
@@ -8,6 +8,8 @@ import DateGroup from "./DateGroup";
 import Message from "./Message";
 import QuickReplies from "./QuickReplies";
 import Announcement from "./Announcement";
+import ApiEventTarget from "../Api/ApiEventTarget";
+import {messages as messagesEvent} from "../Api/Constants/Events";
 
 class Conversation extends Component {
 	constructor(props) {
@@ -17,161 +19,29 @@ class Conversation extends Component {
 
 		// state
 		this.state = {
+			welcomeMessage: "",
 			messages: [],
 			stickyMessage: "",
 		};
 	}
 
 	componentDidMount() {
-		this.getMessages();
-		this.getStickyMessage();
+		// Default the welcome message to the one in the props
+		this.setState(() => ({welcomeMessage: this.props.welcomeMessage}));
+
+		ApiEventTarget.addEventListener(messagesEvent, this.handleMessages);
 	}
 
-	getStickyMessage = () => {
-		const message = "Sorry we are closed right now. We will be open next day from 09:00 - 17:55";
+	componentWillUnmount() {
+		ApiEventTarget.removeEventListener(messagesEvent, this.handleMessages);
+	}
 
-		this.setState(() => ({stickyMessage: message}));
-	};
+	handleMessages = (eventData) => {
+		this.setState(() => ({welcomeMessage: eventData.detail.welcomeMessage}));
 
-	getMessages = () => {
-		const messageArray = [
-			{
-				id: 159299,
-				time: 1623060000,
-				message: null,
-				image: null,
-				typeId: 2,
-				agent: {
-					id: 1,
-					name: "Tracebuzz",
-					avatar: "https://beta.tracebuzz.com/V002/img/avatar.php?i=TB&c=f4931d",
-				},
-				carousel: null,
-				quickReplies: [
-					"yes", "no", "maybe",
-				],
-				custom: null,
-				title: null,
-				media: null,
-				buttons: null,
-			},
-			{
-				id: 159297,
-				time: 1623060000,
-				message: "some_system_message",
-				image: null,
-				typeId: 3,
-				agent: {
-					id: 1,
-					name: "Tracebuzz",
-					avatar: "https://beta.tracebuzz.com/V002/img/avatar.php?i=TB&c=f4931d",
-				},
-				carousel: null,
-				quickReplies: null,
-				custom: null,
-				title: null,
-				media: null,
-				buttons: null,
-			},
-			{
-				id: 159296,
-				time: 1623060000,
-				message: "www.google.nl",
-				image: null,
-				typeId: 2,
-				agent: {
-					id: 1,
-					name: "Tracebuzz",
-					avatar: "https://beta.tracebuzz.com/V002/img/avatar.php?i=TB&c=f4931d",
-				},
-				carousel: null,
-				quickReplies: null,
-				custom: null,
-				title: null,
-				media: null,
-				buttons: null,
-			},
-			{
-				id: 159293,
-				time: 1622800800,
-				message: "😷🤧",
-				image: null,
-				typeId: 1,
-				agent: null,
-				carousel: [],
-				quickReplies: [],
-				custom: [],
-				title: null,
-				media: null,
-				buttons: [],
-			},
-			{
-				id: 159290,
-				time: 1622797200,
-				message: "Test message #4, this one has an emoji 🤧",
-				image: null,
-				typeId: 2,
-				agent: {
-					id: 1,
-					name: "Tracebuzz",
-					avatar: "https://beta.tracebuzz.com/V002/img/avatar.php?i=TB&c=f4931d",
-				},
-				carousel: null,
-				quickReplies: null,
-				custom: null,
-				title: null,
-				media: null,
-				buttons: null,
-			},
-			{
-				id: 159287,
-				time: 1622631600,
-				message: "Test message #3",
-				image: null,
-				typeId: 1,
-				agent: null,
-				carousel: [],
-				quickReplies: [],
-				custom: [],
-				title: null,
-				media: null,
-				buttons: [],
-			},
-			{
-				id: 159284,
-				time: 1622628000,
-				message: "Test message #2, this one has a markdown [link](https://google.com)",
-				image: null,
-				typeId: 2,
-				agent: {
-					id: 1,
-					name: "Tracebuzz",
-					avatar: "https://beta.tracebuzz.com/V002/img/avatar.php?i=TB&c=f4931d",
-				},
-				carousel: null,
-				quickReplies: null,
-				custom: null,
-				title: null,
-				media: null,
-				buttons: null,
-			},
-			{
-				id: 159281,
-				time: 1622545200,
-				message: "Test message #1",
-				image: null,
-				typeId: 1,
-				agent: null,
-				carousel: [],
-				quickReplies: [],
-				custom: [],
-				title: null,
-				media: null,
-				buttons: [],
-			},
-		];
+		this.setState(() => ({messages: eventData.detail.data}));
 
-		this.setState(() => ({messages: messageArray.reverse()}));
+		this.setState(() => ({stickyMessage: eventData.detail.stickyMessage}));
 	}
 
 	setRenderedDate = (date) => {
@@ -213,8 +83,8 @@ class Conversation extends Component {
 			<div className={styles.wrapper}>
 				<div className={styles.body}>
 					{
-						this.props.welcomeMessage
-							&& <Announcement message={this.props.welcomeMessage} />
+						this.state.welcomeMessage
+							&& <Announcement message={this.state.welcomeMessage} />
 					}
 					{
 						this.state.messages.map((message, index, array) => (
