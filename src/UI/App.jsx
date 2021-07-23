e3dbdaf319984e5a3df6563502f3a86ef5e5b596
Gerben-T <gerben.tesselaar@tracebuzz.com>
2021-07-23 09:55:13 +0200

    Move country to runOptions layer

--- src/UI/App.jsx
+++ src/UI/App.jsx
@@ -30,7 +30,7 @@ export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		const interfaceLanguage = window?.parleySettings?.country || "en";
+		const interfaceLanguage = window?.parleySettings?.runOptions?.country || "en";
 		const interfaceTextsDefaults = interfaceLanguage === "nl" ? InterfaceTexts.dutch : InterfaceTexts.english;
 		this.state = {
 			showChat: false,
@@ -277,6 +277,8 @@ export default class App extends React.Component {
 				} else if(path[layer2] === "placeholderMessenger") {
 					objectToSaveIntoState = {interfaceTexts: {inputPlaceholder: value}};
 				}
+			} else if(path[layer1] === "country") {
+				objectToSaveIntoState = {interfaceLanguage: value};
 			}
 		} else if(path[layer0] === "interface") {
 			if(path[layer1] === "hideChatAfterBusinessHours")
@@ -285,8 +287,6 @@ export default class App extends React.Component {
 			objectToSaveIntoState = {accountIdentification: value};
 		} else if(path[layer0] === "authHeader") {
 			objectToSaveIntoState = {deviceAuthorization: value};
-		} else if(path[layer0] === "country") {
-			objectToSaveIntoState = {interfaceLanguage: value};
 		} else if(path[layer0] === "weekdays") {
 			objectToSaveIntoState = {workingHours: value};
 		} else if(path[layer0] === "userAdditionalInformation") {
