cce605d89c448cfadf2de950f20084143ba13d64
Gerben-T <gerben.tesselaar@tracebuzz.com>
2021-07-23 10:24:58 +0200

    Authorization can't be empty when calling subscribeDevice()

--- src/UI/App.jsx
+++ src/UI/App.jsx
@@ -113,11 +113,11 @@ export default class App extends React.Component {
 				undefined,
 				undefined,
 				undefined,
-				nextState.userAdditionalInformation,
+				nextState.userAdditionalInformation || undefined,
 				DeviceTypes.Web,
 				this.state.deviceVersion,
 				undefined,
-				nextState.deviceAuthorization,
+				nextState.deviceAuthorization || undefined,
 			);
 		}
 
