6217f952fa8c370f5348fdfeb9f2bf70d168e9c0
Gerben-T <gerben.tesselaar@tracebuzz.com>
2023-06-12 09:57:19 +0200

    Add styling for close button on ImageViewer component

--- src/UI/ImageViewer.jsx
+++ src/UI/ImageViewer.jsx
@@ -1,14 +1,18 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import * as styles from "./ImageViewer.module.css";
+import {faTimes} from "@fortawesome/free-solid-svg-icons/faTimes";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class ImageViewer extends Component {
 	render() {
+		const iconSize = "3x";
+
 		return (
 			<div className={styles.container}>
 				<img alt={this.props.alt} className={styles.image} src={this.props.src} />
 				<button className={styles.closeButton} onClick={this.props.onClose}>
-					Close
+					<FontAwesomeIcon icon={faTimes} size={iconSize} />
 				</button>
 			</div>
 		);
