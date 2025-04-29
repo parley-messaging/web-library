ca5bc2ae470dc4622ff14544407e8692af991511
Gerben-T <gerben.tesselaar@tracebuzz.com>
2025-04-29 14:43:07 +0200

    Remove todo

--- src/Api/UnreadMessagesCountPolling.js
+++ src/Api/UnreadMessagesCountPolling.js
@@ -1,7 +1,6 @@
 import PollingService from "./Polling";
 
 export default class UnreadMessagesCountPollingService extends PollingService {
-	// TODO: @gerben; make tests
 	async pollFunction() {
 		await this.api.getUnreadMessagesCount();
 	}
