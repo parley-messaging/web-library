7d9b28fcfcf95b60044d1dcb3454aced764f0be8
Gerben-T <gerben.tesselaar@tracebuzz.com>
2025-05-21 13:16:38 +0200

    Adapt ci config to @axe-core/watcher

--- cypress.config.ci.cjs
+++ cypress.config.ci.cjs
@@ -1,21 +1,34 @@
 const {defineConfig} = require("cypress");
+const {cypressConfig} = require("@axe-core/watcher");
 
-module.exports = defineConfig({
-	e2e: {
-		// We've imported your old cypress plugins here.
-		// You may want to clean this up later by importing these.
-		setupNodeEvents(on, config) {
-			return require("./cypress/plugins/index.js")(on, config);
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = defineConfig(
+	cypressConfig({
+		axe: {
+			apiKey: process.env.AXE_API_KEY,
+			runContext: {
+				include: "#app"
+			},
 		},
-		baseUrl: "http://chat-dev.parley.nu:8181",
-		specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-		video: false,
-		reporter: "mochawesome",
-		reporterOptions: {
-			reportFilename: "[name]-report",
-			html: false,
-			json: true,
+		projectId: "wwda8d",
+		e2e: {
+			// We've imported your old cypress plugins here.
+			// You may want to clean this up later by importing these.
+			setupNodeEvents(on, config) {
+				return require("./cypress/plugins/index.js")(on, config);
+			},
+			baseUrl: "http://chat-dev.parley.nu:8181",
+			specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+			video: false,
+			reporter: "mochawesome",
+			reporterOptions: {
+				reportFilename: "[name]-report",
+				html: false,
+				json: true,
+			},
+			retries: {runMode: 2},
 		},
-		retries: {runMode: 2},
-	},
-});
+	}),
+);
